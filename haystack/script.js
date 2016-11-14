13ffd24a1cd88213c1c30bc7ed8d57587aee19e3
Jeremy Arimado <jarimado@gmail.com>
2016-11-15 07:04:38 +1100

    added mobile/responsive

--- haystack/script.js
+++ haystack/script.js
@@ -5,8 +5,8 @@ const state = {
 // -----------------------------------------------------------------------------
 
 var concatenateValuesOf = function ( obj ) {
-  let result = ''
-  for (let key in obj) {
+  var result = ''
+  for (var key in obj) {
     result += `${obj[key].reduce(function(p, c) { return p += " " + c + " " })}`
   }
   return result
@@ -29,7 +29,6 @@ var cardElements = cards.map(createCard)
 
 // -----------------------------------------------------------------------------
 
-
 // d_ : dom elements
 // s_ : side effects
 
@@ -40,19 +39,19 @@ var s_resetCards = function (cardElements) {
   })
 }
 
-var animationDoneCB = function ( element, index, elements ) {
+var s_animationDoneCB = function ( element, index, elements ) {
   return function( e ) {
     if ( elements.length - 1 === index ) {
       s_resetCards(elements);
-      animateCards();
+      s_animateCards();
     }
   }
 }
 
 var d_cards = document.querySelectorAll('.card')
 
-var animateCards = function () {
-  d_cards.forEach(function(card, idx) {
+var s_animateCards = function () {
+  Array.prototype.forEach.call(d_cards, function(card, idx) {
     var cardNo = idx + 1;
     setTimeout(function () {
       if ( idx % 2 === 0 ) {
@@ -60,16 +59,15 @@ var animateCards = function () {
       } else {
         card.classList.add('slide-in-left')
       }
-      card.addEventListener("webkitAnimationEnd", animationDoneCB(card, idx, d_cards),false);
-      card.addEventListener("animationend", animationDoneCB(card, idx, d_cards),false);
-      card.addEventListener("oanimationend", animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("webkitAnimationEnd", s_animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("animationend", s_animationDoneCB(card, idx, d_cards),false);
+      card.addEventListener("oanimationend", s_animationDoneCB(card, idx, d_cards),false);
     }, cardNo * 5000);
-  });
-}
-
-animateCards();
+  })
+} 
 
 
+s_animateCards();
 
 // add slide toggle in
 // no repeat
